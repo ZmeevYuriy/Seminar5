d0ae880ff91ce6b85dc1767c13f9bec089d6b963
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-18 23:44:36 +0600

    add to Task_38 correct

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.WriteLine("Введите размер массива");
 int N = Convert.ToInt32(Console.ReadLine());
 double[] numbers = new double[N];
 FillArrayRandomNumbers(numbers);
